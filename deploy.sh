0363ab397cb9d1b7f0101543eaef7686b21df447
marco <marco.w.rademan@gmail.com>
2026-06-20 13:05:20 +0200

    Added deploy script and made the resource link queryable for a quick check

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-# TODO: SSH-based deployment
+# exit on non-zero status
+set -e
+
+echo "SSH-ing in..."
+ssh psimmetry-leadership
+
+echo "Navigating to repo..."
+cd ~/leaderhip-simulation
+
+echo "Pulling main"
+git pull
+
+echo "Cleaning ~/htdocs"
+rm -rf ~/htdocs
+
+echo "Copying new ./htdocs files to ~/htdocs"
+cp -r ./htdocs ~/htdocs
+
+echo "Finished!
+exit 1
--- htdocs/public/index.php
+++ htdocs/public/index.php
@@ -8,18 +8,31 @@ $RESOURCE_PATH = $PATH . '/resources';
 require_once $PATH  . "/src/links.php";
 require_once $PATH  . "/src/config.php";
 
+$query = false;
+if (isset($_GET['query']))  {
+    $query = true;
+}
+
 $verify = verifyLink();
 if(!isset($verify['resource'])) {
-    $contents = file_get_contents($RESOURCE_PATH . '/invalid.html');
-    // echo type($verify);
-    // echo implode(", ", $verify);
-    // echo implode(", ", array_keys($verify));
-    $contents = str_replace('{placeholder}', $verify["status"], $contents);
-    echo $contents;
+    if($query) {
+        http_response_code(403);
+        echo 'invalid';
+    } else {
+        $contents = file_get_contents($RESOURCE_PATH . '/invalid.html');
+        // echo type($verify);
+        // echo implode(", ", $verify);
+        // echo implode(", ", array_keys($verify));
+        $contents = str_replace('{placeholder}', $verify["status"], $contents);
+        echo $contents;
+    }
 } else {
-    // $contents = file_get_contents($RESOURCE_PATH . '/' . $verify['resource']);
-    // echo $contents;
-    include $RESOURCE_PATH . '/' . $verify['resource'];
+    if($query) {
+        http_response_code(200);
+        echo 'ok';
+    } else {
+        include $RESOURCE_PATH . '/' . $verify['resource'];
+    }
 }
 
 ?>
